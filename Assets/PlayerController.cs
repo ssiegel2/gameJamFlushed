d94af4083e13c8085f904e454587a60301bcf2ca
Sam Siegel <ssiegel2@umbc.edu>
2016-09-10 01:21:40 -0400

    Slower movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,22 +3,24 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed = 20f;
+    public float moveAmount = 20f;
+    public float maxSpeed = 5f;
 
     Rigidbody2D rigidBody;
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody2D>();
-        rigidBody.AddForce(new Vector2(0, moveSpeed));
+        rigidBody.AddForce(new Vector2(0, moveAmount));
 	}
 	
     void FixedUpdate(){
-        float hMove = Input.GetAxis("Horizontal");
-        float vMove = Input.GetAxis("Vertical");
+        float hMove = Input.GetAxis("Horizontal") * moveAmount;
+        float vMove = Input.GetAxis("Vertical") * moveAmount;
 
-        rigidBody.AddForce(new Vector2(hMove * moveSpeed, vMove * moveSpeed));
+        rigidBody.AddForce(new Vector2(Mathf.Clamp(hMove, -maxSpeed, maxSpeed), Mathf.Clamp(vMove, -maxSpeed, maxSpeed)));
 
-        
+
+        Debug.Log(rigidBody.velocity);
     }
 
 
