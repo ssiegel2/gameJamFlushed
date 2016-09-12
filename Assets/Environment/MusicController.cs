17421d8a0013de935f8256b37f1bfaea0cccbd3a
Sam Siegel <ssiegel2@umbc.edu>
2016-09-11 20:15:22 -0400

    Adding music in game (in progress)

---
+++ Assets/Environment/MusicController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicController : MonoBehaviour {
+
+    GameStateScript gameState;
+    AudioSource gameMusic;
+
+    Dictionary<string, AudioClip> music;
+
+	// Use this for initialization
+	void Start () {
+        gameState = GameObject.Find("GameState").GetComponent<GameStateScript>();
+        gameMusic = GetComponent<AudioSource>();
+
+        music = new Dictionary<string, AudioClip>();
+        music.Add("FlushedTestSong1", Resources.Load("FlushedTestSong1") as AudioClip);
+        music.Add("FlushedTestSong2", Resources.Load("FlushedTestSong2") as AudioClip);
+
+        Debug.Log(Resources.Load("FlushedTestSong1"));
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(music["FlushedTestSong1"]);
+
+        switch (gameState.CurrentState())
+        {
+            case "Overworld":
+                gameMusic.clip = music["FlushedTestSong1"];
+                break;
+            case "Dialogue":
+                gameMusic.clip = music["FlushedTestSong2"];
+                break;
+        }
+        Debug.Log(gameMusic.clip);
+        gameMusic.Play();
+    }
+}
