9600e9e3b63159a8623263708dcb51e21be2c5ad
Sam Siegel <ssiegel2@umbc.edu>
2016-09-23 16:40:34 -0400

    Started toilet minigame, brush movement

---
+++ Assets/Objects/Toilet/BrushController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrushController : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 10f;
+
+    bool rotating = true;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+    void FixedUpdate () {
+
+        if (rotating) {
+            transform.Rotate(new Vector3(0, 0, 1), Time.deltaTime * rotateSpeed);
+            Vector2 forceToAdd = transform.rotation * -Vector2.up * (moveSpeed);
+
+            rigidBody.AddForce(transform.rotation * -Vector2.up * moveSpeed);
+        }
+        else {
+            rigidBody.AddForce(transform.rotation * Vector2.up * moveSpeed);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            rotating = !rotating;
+        }
+	}
+}
